1704223e27c0c880027ca7011c322ff8ed0094c7
Julia <julia.areal@sou.ufac.br>
2025-09-04 11:47:53 -0500

    feat: launch.json e debug tools

--- sistema_racks/inventario/views.py
+++ sistema_racks/inventario/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.forms import AuthenticationForm
 from .models import Rack, Switch
 from .forms import RackForm, SwitchForm
 
+# resquest handlers
+
 # ---------------------------
 # Home
 # ---------------------------
--- sistema_racks/sistema_racks/urls.py
+++ sistema_racks/sistema_racks/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("inventario.urls")),  # envia tudo para o app inventario
+    path("inventario/", include("inventario.urls")), # envia tudo para o app inventario
+    path("__debug__/", include(debug_toolbar.urls)),
 ]
